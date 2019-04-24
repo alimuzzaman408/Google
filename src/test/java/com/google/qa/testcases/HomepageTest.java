227dce37893c026e699bfd6f2f8ddb5d082cb378
alimuzzaman <alimuzzaman408@gmail.com>
2019-04-24 12:54:58 -0400

    new class created

---
+++ src/test/java/com/google/qa/testcases/HomepageTest.java
@@ -0,0 +1,29 @@
+package com.google.qa.testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HomepageTest {
+	
+	
+	
+	
+	@BeforeMethod
+	public void setUp(){
+		
+		
+	}
+	@Test
+	public void test(){
+		
+		
+		
+	}
+@AfterMethod
+public void tearDown(){
+	
+	
+	
+}
+}
---
+++ src/test/java/com/google/qa/testcases/LoginPageTest.java
@@ -0,0 +1,25 @@
+package com.google.qa.testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	@BeforeMethod
+	public void setUp(){
+		
+		
+	}
+	@Test
+	public void test(){
+		
+		
+		
+	}
+@AfterMethod
+public void tearDown(){
+	
+	
+	
+}
+}
